1392fc48735cfab7be5a2a7f8bf416c202208f3a
Tobias Zaenker <tzaenker@informatik.uni-bonn.de>
2023-10-01 17:11:17 +0200

    Update Open3D version

--- install_open3d.sh
+++ install_open3d.sh
@@ -1,5 +1,5 @@
-filename=open3d-devel-linux-x86_64-cxx11-abi-cuda-0.16.0
-wget -nc https://github.com/isl-org/Open3D/releases/download/v0.16.0/$filename.tar.xz
+filename=open3d-devel-linux-x86_64-cxx11-abi-cuda-0.17.0
+wget -nc https://github.com/isl-org/Open3D/releases/download/v0.17.0/$filename.tar.xz
 tar -xvf $filename.tar.xz 
 sudo rsync -rvP $filename/* /usr/local
 rm -r $filename
